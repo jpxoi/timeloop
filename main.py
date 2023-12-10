4acb87e58d6949a02e035173aaeef89f3038233b
Jean Paul Fernandez <jpxoi@icloud.com>
2023-12-10 09:59:39 +0000

    Change Flask app variable name to uppercase

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flask import Flask, request, jsonify
 
-app = Flask(__name__)
+APP = Flask(__name__)
 
-@app.route('/')
+@APP.route('/')
 def home():
     return "Hello World!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    APP.run(debug=True)
