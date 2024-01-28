937a2e1a1c76ed19327922362e89c3a732f11884
Jean Paul Fernandez <jpxoi@icloud.com>
2024-01-28 18:45:37 +0000

    Add Flask-RESTful API endpoint

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, request, jsonify
+from flask import Flask
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
 app.config.from_pyfile('settings.py')
+api = Api(app)
 
-@app.route('/')
-def home():
-    return "Hello World!"
+class HelloWorld(Resource):
+    def get(self):
+        return {"Hello World" : "Hello World"}
+    
+api.add_resource(HelloWorld, '/')
 
 if __name__ == '__main__':
     app.run()
