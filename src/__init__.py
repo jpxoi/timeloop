51c686d1d5a48e7151e2fac53927359b6a902540
Jean Paul Fernandez <jpxoi@icloud.com>
2024-01-29 10:51:22 +0000

    Add Flask app initialization and routing
    This commit adds the necessary code to initialize a Flask app and register the required routes for the application.

---
+++ src/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from .routes import IndexRoutes, UsersRoutes
+
+app = Flask(__name__)
+
+def error404(error):
+    return {'message': 'Not found'}, 404
+
+def init_app(config):
+    app.config.from_object(config)
+
+    app.register_error_handler(404, error404)
+
+    app.register_blueprint(IndexRoutes.main, url_prefix='/')
+    app.register_blueprint(UsersRoutes.main, url_prefix='/users')
+
+    return app
