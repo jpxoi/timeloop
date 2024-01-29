49fa8c1535f536f454e496989bc2615db96943fd
Jean Paul Fernandez <jpxoi@icloud.com>
2024-01-29 11:04:17 +0000

    Refactor app.py to use a variable for configuration

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from config import config
 from src import init_app
 
-app = init_app(config['development'])
+configuration = config['development']
+app = init_app(configuration)
 
 if __name__ == '__main__':
     app.run()
