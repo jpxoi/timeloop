1891cb22f0ae11bf03c5c23e9f870ef54e8aa5a3
Jean Paul Fernandez <jpxoi@icloud.com>
2024-01-29 11:16:03 +0000

    Add VideosRoutes to app registration

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from .routes import IndexRoutes, UsersRoutes
+from .routes import IndexRoutes, UsersRoutes, VideosRoutes
 
 app = Flask(__name__)
 
@@ -13,5 +13,6 @@ def init_app(config):
 
     app.register_blueprint(IndexRoutes.main, url_prefix='/')
     app.register_blueprint(UsersRoutes.main, url_prefix='/users')
+    app.register_blueprint(VideosRoutes.main, url_prefix='/videos')
 
     return app
