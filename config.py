a0cdef1ec84f41c65b476299739ae42fbdc80d1c
Jean Paul Fernandez <jpxoi@icloud.com>
2024-01-29 10:31:38 +0000

    Refactor config.py to separate DevConfig and ProdConfig classes

--- config.py
+++ config.py
@@ -1,11 +1,26 @@
 import os
 
-# Access the environment variables from the .env file
-MYSQL_HOST = os.getenv("MYSQL_HOST")
-MYSQL_PORT = int(os.getenv("MYSQL_PORT"))
-MYSQL_USER = os.getenv("MYSQL_USER")
-MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
-MYSQL_DB = os.getenv("MYSQL_DB")
-MYSQL_CHARSET = os.getenv("MYSQL_CHARSET")
+class DevConfig():
+    # Access the environment variables from the .env file
+    MYSQL_HOST = os.getenv("MYSQL_HOST")
+    MYSQL_PORT = int(os.getenv("MYSQL_PORT"))
+    MYSQL_USER = os.getenv("MYSQL_USER")
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+    MYSQL_DB = os.getenv("MYSQL_DB")
+    MYSQL_CHARSET = os.getenv("MYSQL_CHARSET")
 
+
+class ProdConfig():
+    # Access the environment variables from the .env file
+    MYSQL_HOST = os.getenv("MYSQL_HOST")
+    MYSQL_PORT = int(os.getenv("MYSQL_PORT"))
+    MYSQL_USER = os.getenv("MYSQL_USER")
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+    MYSQL_DB = os.getenv("MYSQL_DB")
+    MYSQL_CHARSET = os.getenv("MYSQL_CHARSET")
+
+config = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
 # PLEASE DO NOT MODIFY THIS FILE WITHOUT THE PERMISSION OF THE BACKEND TEAM LEADER
