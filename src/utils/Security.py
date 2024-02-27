0aa0ccaeca69f3e9e608213ce48a3871f5948b13
Jean Paul Fernandez <jpxoi@icloud.com>
2024-02-27 01:47:20 +0000

    Add Security.py for JWT token generation

---
+++ src/utils/Security.py
@@ -0,0 +1,22 @@
+from decouple import config
+import datetime
+import pytz
+import jwt
+
+class Secyrity():
+
+    tz = pytz.timezone('Europe/London')
+    secret = config('JWT_KEY')
+
+    @classmethod
+    def generate_token(cls, authenticated_user):
+        payload = {
+            'iat': datetime.datetime.now(tz=cls.tz),
+            'exp': datetime.datetime.now(tz=cls.tz) + datetime.timedelta(minutes=1440),
+            'username': authenticated_user.username,
+            'first_name': authenticated_user.first_name,
+            'last_name': authenticated_user.last_name,
+            'email': authenticated_user.email,
+        }
+
+        return jwt.encode(payload, cls.secret, algorithm='HS256')
