e1fae54919b978c48142e09fe5d5d4d711f2e2f2
Jean Paul Fernandez <jpxoi@icloud.com>
2024-02-27 01:00:47 +0000

    Delete calendar and event routes, update user routes

--- src/routes/UsersRoutes.py
+++ src/routes/UsersRoutes.py
@@ -2,14 +2,28 @@ from flask import Blueprint, jsonify
 
 main = Blueprint('users_blueprint', __name__)
 
-@main.route('/')
+@main.route('/', methods=['GET'])
 def users():
     try:
-        return jsonify({'message': 'This is the users route'})
+        return jsonify({'message': 'This is the users route. It should return all users'})
+    except Exception as e:
+        return jsonify({'message': str(e)})
+    
+@main.route('/<int:id>', methods=['GET'])
+def user(id):
+    try:
+        return jsonify({'message': 'This is the user route. It should return user information with the given id.', 'id': id})
+    except Exception as e:
+        return jsonify({'message': str(e)})
+    
+@main.route('/<int:id>', methods=['PUT'])
+def user(id):
+    try:
+        return jsonify({'message': 'This is the user route. It should update user information with given id.', 'id': id})
     except Exception as e:
         return jsonify({'message': str(e)})
 
-@main.route('/<int:id>')
+@main.route('/<int:id>', methods=['DELETE'])
 def user(id):
     try:
         return jsonify({'message': 'This is the user route', 'id': id})
