9bae96aed61c5d31e4d9ad43f58713be6cc235e5
Jean Paul Fernandez <jpxoi@icloud.com>
2024-02-27 15:25:54 +0000

    Add signup functionality and bcrypt password hashing

--- src/routes/AuthRoutes.py
+++ src/routes/AuthRoutes.py
@@ -1,24 +1,34 @@
 from flask import Blueprint, request, jsonify
+import bcrypt
+from src.services.AuthService import AuthService
 
 main = Blueprint('auth_blueprint', __name__)
 
 @main.route('/signup', methods=['POST'])
 def signup():
-    try:
-        username = request.json['username']
-        first_name = request.json['first_name']
-        last_name = request.json['last_name']
-        email = request.json['email']
-        password = request.json['password']
+    username = request.json['username']
+    first_name = request.json['first_name']
+    last_name = request.json['last_name']
+    email = request.json['email']
+    password = request.json['password']
 
-        return jsonify({'message': 'This is the signup route. It should create a new user'})
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+    try:
+        # Create new user in the database
+        # user = User(username, first_name, last_name, email, hashed_password)
+        # user.save()
+        
+        # Return if successful
+        return jsonify({'message': 'User created successfully'})
     except Exception as e:
         return jsonify({'message': str(e)})
     
 @main.route('/login', methods=['POST'])
 def login():
     try:
-        return jsonify({'message': 'This is the login route. It should return a token'})
+        return jsonify({'message': 'This is the login route. It should login the user'})
     except Exception as e:
         return jsonify({'message': str(e)})
     
