f4f1d6517d048abfb815e23b6ce9157355e26221
Jean Paul Fernandez <jpxoi@icloud.com>
2024-02-27 01:00:43 +0000

    Update route prefixes for API versioning

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from .routes import IndexRoutes, UsersRoutes, CalendarsRoutes, EventsRoutes, VideosRoutes
+from .routes import IndexRoutes, CalendarsRoutes, EventsRoutes, VideosRoutes
 
 app = Flask(__name__)
 
@@ -12,9 +12,8 @@ def init_app(config):
     app.register_error_handler(404, error404)
 
     app.register_blueprint(IndexRoutes.main, url_prefix='/')
-    app.register_blueprint(UsersRoutes.main, url_prefix='/users')
-    app.register_blueprint(CalendarsRoutes.main, url_prefix='/calendars')
-    app.register_blueprint(EventsRoutes.main, url_prefix='/events')
-    app.register_blueprint(VideosRoutes.main, url_prefix='/videos')
+    app.register_blueprint(CalendarsRoutes.main, url_prefix='/api/v1/calendars')
+    app.register_blueprint(EventsRoutes.main, url_prefix='/api/v1/events')
+    app.register_blueprint(VideosRoutes.main, url_prefix='/api/v1/videos')
 
     return app
