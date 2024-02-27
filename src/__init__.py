82bf4fb4e025f3963de0ff5fa63354efdb62b34e
Jean Paul Fernandez <jpxoi@icloud.com>
2024-02-27 01:47:08 +0000

    Add CORS support for localhost:5173

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from .routes import IndexRoutes, AuthRoutes, UsersRoutes, TimezonesRoutes, VideosRoutes
 
 app = Flask(__name__)
+
+CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
 
 def error404(error):
     return {'message': 'Not found'}, 404
