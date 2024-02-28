5f58073f659b5554e1011caca9682c5a8e8ccb2b
Jean Paul Fernandez <jpxoi@icloud.com>
2024-02-28 16:03:28 +0000

    Update index route message

--- src/routes/IndexRoutes.py
+++ src/routes/IndexRoutes.py
@@ -7,6 +7,6 @@ main = Blueprint('index_blueprint', __name__)
 @main.route('/')
 def index():
     try:
-        return jsonify({'message': 'This is the index route.'})
+        return jsonify({'status': 'success', 'message': 'You have reached the index route! To learn more about the available routes, visit the documentation at...'})
     except Exception as e:
         return jsonify({'message': str(e)})
