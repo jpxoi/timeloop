29936ff22fccbc83ca96a576c7bd0b6f56dacb4a
Jean Paul Fernandez <jpxoi@icloud.com>
2024-02-29 15:48:30 +0000

    Update index route message

--- src/routes/IndexRoutes.py
+++ src/routes/IndexRoutes.py
@@ -9,7 +9,7 @@ def index():
     try:
         data = {
             'status': 'success',
-            'message': 'You have reached the index route! To learn more about the available routes, visit the documentation at...'
+            'message': 'You have reached the index route! To learn more about the available routes, request the documentation to your team leader.'
         }
         return data, 200
     except Exception as e:
