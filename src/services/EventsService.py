2a3e1398891dd1c550616f02c708f8429673f0cc
Jean Paul Fernandez <jpxoi@icloud.com>
2024-03-05 10:26:35 +0000

    Add new methods to EventsService.py

--- src/services/EventsService.py
+++ src/services/EventsService.py
@@ -5,11 +5,11 @@ from random import randint
 class CalendarService():
 
     @classmethod
-    def get_user_calendar_events(cls, user_id):
+    def new_event(cls, user_id, calendar_id, event_name, event_description, event_start, event_end):
         pass
 
     @classmethod
-    def new_event(cls, user_id, calendar_id, event_name, event_description, event_start, event_end):
+    def get_events(cls, user_id):
         pass
 
     @classmethod
@@ -20,4 +20,27 @@ class CalendarService():
     def update_event(cls, user_id, calendar_id, event_id, event_name, event_description, event_start, event_end):
         pass
 
-    
+    @classmethod
+    def delete_event(cls, user_id, calendar_id, event_id):
+        pass
+
+    @classmethod
+    def event_id_exists(cls, event_id):
+        try:
+            connection = get_connection()
+            with connection.cursor() as cursor:
+                sql = "SELECT * FROM events WHERE event_id = %s"
+                cursor.execute(sql, (event_id))
+                result = cursor.fetchone()
+            connection.close()
+
+            if result:
+                return True
+            else:
+                return False
+
+        except Exception as e:
+            return {
+                'status': 'error',
+                'message': str(e)
+            }
